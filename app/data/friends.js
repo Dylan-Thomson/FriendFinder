42f5eaf2575d97943267f0176298d83be44de25b
Dylan Thomson <djthomson88@gmail.com>
2019-02-02 16:26:08 -0500

    Survey questions, friend objects, styling

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,21 +1,55 @@
 // STORE FRIEND OBJECTS HERE
 const friends = [
     {
-        "name":"Ahmed",
-        "photo":"https://cook.fnr.sndimg.com/content/dam/images/cook/editorial/talent/guy-fieri/CCTV-TalentAvatar-Guy-Fieri-800x800.jpg.rend.hgtvcom.616.616.suffix/1478703698614.jpeg",
-        "scores":[
+        name:"Bob Ross",
+        photo:"https://images05.military.com/sites/default/files/styles/full/public/media/people/2013/05/bobrosspainting.jpg?itok=A3G_e8MM",
+        scores:[
             5,
             1,
             4,
+            1,
+            5,
+            1,
+            3,
+            1,
             4,
+            4
+          ]
+    },
+    {
+        name:"Carl Sagan",
+        photo:"https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Carl_Sagan_Planetary_Society.JPG/220px-Carl_Sagan_Planetary_Society.JPG",
+        scores:[
+            5,
+            3,
+            3,
+            2,
+            4,
+            3,
+            5,
+            3,
+            1,
+            3
+          ]
+    },
+    {
+        name:"Gordon Ramsay",
+        photo:"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Gordon_Ramsay.jpg/220px-Gordon_Ramsay.jpg",
+        scores:[
+            2,
             5,
             1,
             2,
+            1,
+            5,
             5,
             4,
+            1,
             1
           ]
-      }      
+    },
+
+
 ];
 
 module.exports = friends;
